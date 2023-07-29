a9ed39f7b9f4c969e4a75779e8bf7e0333ec93fd
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-29 10:58:06 -0700

    array js

---
+++ array.js
@@ -0,0 +1,9 @@
+// array decliatrion
+var friendsAge = [14, 11, 45, 56, 34, 9];
+var picnicFee = [4000, 5600, 4570, 6322, 150];
+
+var nayikas = ['mahi', 'opu', 'sabana', 'sabnur'];
+console.log(nayikas);
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log(vowels.length);
---
+++ nood.js
@@ -0,0 +1,3 @@
+// Array delicer 
+var friendsAge = [14, 11, 45, 56, 34, 9];
+console.log(friendsAge.length);
