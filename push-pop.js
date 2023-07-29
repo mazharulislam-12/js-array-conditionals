c52f5755208eac268ffc3f24b8d1b7a812c2a2db
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-29 12:15:33 -0700

    push and pop

---
+++ push-pop.js
@@ -0,0 +1,19 @@
+var numbers = [45, 56, 85, 78, 96];
+numbers.push(67);
+console.log(numbers);
+
+// use pop to added element to an array as the last element 
+var names = ['anik', 'rana', 'hanif', 'jon', 'deo']
+names.push('kalam');
+names.push('khan');
+console.log(names);
+
+// use pop to get last element 
+var num1 = [45, 58, 63, 87];
+num1.pop()
+var element = num1.pop()
+console.log(num1);
+
+
+
+
