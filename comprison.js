01f209fc74ea5c417b420958124046dd3be42eb5
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-29 13:20:04 -0700

    conprison for js

---
+++ comprison.js
@@ -0,0 +1,26 @@
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(6 == 6);
+console.log(5 != 6);
+console.log(15 != 15);
+// less then ot equal
+console.log(5 <= 5);
+console.log(16 <= 6);
+
+// greater then or equal
+console.log(5 >= 6);
+console.log(5 >= 2);
+console.log(2 >= 2);
+
+var num1 = 8;
+var num2 = 10;
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 == num2);
+console.log(num1 != num2);
+
+
+// multiple condition
+// money1 > money2 && result1 > result2 && height1 > height2
+// money1 > money2 || result1 > result2 || height1 > height2
