62ec1bb1146c6999e31356b0509bf0f1572268e8
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-29 11:56:11 -0700

    indes of js

---
+++ index.js
@@ -0,0 +1,14 @@
+var numbers = [45, 75,86, 63, 66, 98];
+// 1. get element by index 
+var element = numbers[5];
+
+//2. set element value by index
+numbers[1] = 77;
+numbers[5] = 85;
+console.log(numbers);
+
+// 3.find index of an element
+var positionIndex = numbers.indexOf(86);
+console.log(positionIndex);
+
+console.log(element);
--- nood.js
+++ nood.js
@@ -1,3 +1,22 @@
 // Array delicer 
 var friendsAge = [14, 11, 45, 56, 34, 9];
 console.log(friendsAge.length);
+
+// adday index : 0,1,2,3,4,.........
+var numbers = [45, 75,86, 63, 66, 98];
+//  get element by index 
+var element = numbers[5];
+
+//1. set element value by index
+numbers[1] = 77;
+numbers[5] = 85;
+console.log(numbers);
+
+//2. get element by index 
+var element = numbers[5];
+console.log(element);
+
+// 3.find index of an element
+var positionIndex = numbers.indexOf(86);
+console.log(positionIndex);
+
