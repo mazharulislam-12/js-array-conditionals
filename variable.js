fe66493a8451b2aa68236a400a6bc42f80301ccd
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-29 10:29:07 -0700

    varibale js

---
+++ variable.js
@@ -0,0 +1,19 @@
+// number type variable 
+var sunglassPrice = 120;
+
+// string type variable 
+var specialOneName = "Ali Bat";
+
+// boolean type variable
+var isSingle = true;
+
+// two types of number 
+// Integer :120;
+// float, decimal : 3.65;
+
+// math operations : +, -, *, /
+// shorthand : +=, -=, ++, --
+
+// convert: ParseeInt , ParseFloat
+// tofixed
+// remainder %
