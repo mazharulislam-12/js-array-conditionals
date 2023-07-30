6b4fbc3e2b2afb71ae23fbe37b736505cdc1b643
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-30 00:11:28 -0700

    summary of js

---
+++ multi-stage-condition.js
@@ -0,0 +1,17 @@
+var money = 10;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if (danishPrice < money) {
+    console.log("I eat danish");
+}
+else if(butterBread < money){
+    console.log("I eat Bread");
+}
+else if(toastBiscuit < money){
+    console.log("I eat toast");
+}
+else{
+    console.log("I eat tea");
+}
---
+++ summar.js
@@ -0,0 +1,8 @@
+var destinations = ['sajak', 'coxsbaazar', 'napal', 'bali',];
+console.log(destinations[2]);
+destinations[2] = 'thiland';
+var index = destinations.indexOf ('bali');
+destinations.length
+destinations.push('button');
+destinations.pop()
+
