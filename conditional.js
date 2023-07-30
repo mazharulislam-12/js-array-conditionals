03459d8aa8798f8a045e701710bd994096fedbb8
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-29 22:45:40 -0700

    condition for js

---
+++ conditional.js
@@ -0,0 +1,34 @@
+var iphonePrice = 79000;
+var myBudge = 95000;
+// if iphone price is less then my budget. I will buy the iphone 
+/**
+ * if iphone price is less then my budge 
+ * I will buy the iPhone 
+ *      
+ *      if (condition ){
+ *              will execute if the condition id true
+ *          }
+ * */ 
+
+if (iphonePrice < myBudge){
+    console.log("I whill buy the Iphone");
+}
+
+
+var chickenPrice = 500;
+var myMoney = 50;
+
+// if (chickenPrice <= myMoney){
+//     console.log("I will eat chicken");
+// }
+// if (chickenPrice > myMoney) {
+//     console.log("I will eat smashed potato and lentils sup");
+// }
+
+
+if (chickenPrice < myMoney) {
+    console.log("I will eat checken");
+}
+else{
+    console.log("I will eat Potato");
+}
