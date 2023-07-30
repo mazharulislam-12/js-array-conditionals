72c4bfddb2d45dc6ddff66ccbb768565aee87821
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-29 23:31:09 -0700

    multiple conditon for javaScript

---
+++ multiple-condition.js
@@ -0,0 +1,48 @@
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 0;
+
+/**
+ * if (isGraduated == true) {
+    console.log("Eso biya kora feli");
+}
+else{
+    console.log("Tor kopal a biya nai");
+}
+
+ */
+/**
+ * 
+if (isGraduated == true && salary > 50000) {
+    console.log("Eso biya kora feli");
+}
+else{
+    console.log("Tor kopal a biya nai");
+}
+ */
+
+
+
+// if (isGraduated == true && salary > 50000 && cars >= 1) {
+//     console.log("Eso biya kora feli");
+// }
+// else{
+//     console.log("Tor kopal a biya nai");
+// }
+
+
+var isGraduated = false;
+var salary = 51000;
+var cars = 1;
+
+if (isGraduated === true || salary > 50000 || cars >= 1) {
+    console.log("Esho prem kori");
+}
+else{
+    console.log("Tmer friend er number dau");
+}
+
+if ((isGraduated === true && salary > 5000) || cars >=1) {
+    console.log("Biya korba");
+    
+}
